22269d51a7b2a4de7a0d25a9503ddb700388adeb
AndrMJ <48027984+AndrMJ@users.noreply.github.com>
2019-06-04 18:41:14 +0000

    Initial sync

---
+++ app/config/assets/commonDefinitions.js
@@ -0,0 +1,38 @@
+window.odkCommonDefinitions = {
+  "_tokens": {},
+  "_locales": {
+    "setting_name": "_locales",
+    "_row_num": 5,
+    "value": [
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "default": "English",
+              "portuguese": "Inglês"
+            }
+          }
+        },
+        "_row_num": 6,
+        "name": "default"
+      },
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "default": "Portuguese",
+              "portuguese": "Português"
+            }
+          }
+        },
+        "_row_num": 7,
+        "name": "portuguese"
+      }
+    ]
+  },
+  "_default_locale": {
+    "setting_name": "_default_locale",
+    "_row_num": 5,
+    "value": "default"
+  }
+}
