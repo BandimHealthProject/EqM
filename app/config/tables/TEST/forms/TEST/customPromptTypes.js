afa0bb9d18f7486c7ac93d791db3a5df3d04e367
CarlRossing <52914866+CarlRossing@users.noreply.github.com>
2019-07-30 13:49:04 +0200

    small update

--- app/config/tables/TEST/forms/TEST/customPromptTypes.js
+++ app/config/tables/TEST/forms/TEST/customPromptTypes.js
@@ -244,7 +244,7 @@ var async_assign = promptTypes.base.extend({
     type: "async_assign",
     debug: false,
     valid: true,
-    templatePath: '../config/tables/CRIANCA/forms/CRIANCA/templates/async_assign.handlebars',
+    templatePath: '../config/tables/TEST/forms/TEST/templates/async_assign.handlebars',
     _cachedSelection: null,
     getLinkedTableId: function() {
         var queryDefn = opendatakit.getQueriesDefinition(this.values_list);
